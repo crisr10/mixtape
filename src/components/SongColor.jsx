6c2a0cff48ca965f97a95c680f232cfbe6fc2836
Bryce Richards <brycerichards@Bryces-MacBook-Pro.local>
2017-05-25 18:59:42 -0400

    added react-color, updated SongColor component

--- src/components/SongColor.jsx
+++ src/components/SongColor.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
+import { HuePicker } from "react-color";
 
 class SongColor extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            color: "",
+            color: "FFF",
         };
+    }
+
+    updateColor(color) {
+        this.setState({
+            color: color.hex
+        })
     }
 
     render() {
         return (
             <div>
+                <HuePicker
+                    color={this.state.color}
+                    onChangeComplete={this.updateColor}
+                />
             </div>
         );
     }
